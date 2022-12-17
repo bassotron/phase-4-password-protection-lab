3b4028eb1a7c4581d559b63c6ebdf4b0120877ed
Will Lovell <91235913+bassotron@users.noreply.github.com>
2022-12-17 14:52:13 -0700

    commit tree fcc8079ac8e8635a559f252f4d811df57646d5f0

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+	before_action :authorize, only: [:show]
+
+def show
+	user = User.find_by(id: session[:user_id])
+	render json: user
+
+end
+
+
+	def create
+		user = User.create(user_params)
+		session[:user_id] = user.id
+		if user.valid?
+			render json: user, status: :created
+		  else
+			render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+		  end
+	end
+
+
+
+	private
+	def authorize
+		render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+	end
+
+
+	def user_params
+		params.permit(:username, :password, :password_confirmation)
+	end
+
+end
